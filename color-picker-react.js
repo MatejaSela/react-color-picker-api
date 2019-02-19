3a441da603148c25086ffffbf6297ff3c7a5a908
Christopher Andrews <candrews@middlebury.edu>
2019-02-18 22:03:15 -0500

    Adds basic color picker

--- color-picker-react.js
+++ color-picker-react.js
@@ -32,8 +32,40 @@ class LabeledSlider extends React.Component {
     );
   }
 }
+
+class ColorPicker extends React.Component {
+  constructor(props){
+    super(props);
+    
+  }
+
+  render() { 
+    const swatchProps = {
+      style: {
+        width: '100px',
+        height: '100px',
+        border: '1px solid black',
+        background: 'rgb(0, 0, 0)',
+      },
+    };
+
+
+    return React.createElement(
+      'div', null,
+      React.createElement('div', swatchProps),
+      React.createElement(LabeledSlider, { label: 'Red' }),
+      React.createElement(LabeledSlider, { label: 'Green' }),
+      React.createElement(LabeledSlider, { label: 'Blue' }),
+    )
+  }  
+}
+
+
+
+
+
       
 ReactDOM.render(
-  React.createElement(LabeledSlider, {label: 'red'}, null),
+  React.createElement(ColorPicker),
     document.getElementById('root')
 );
